ff49747721852a33c8fe97d0b3a97e0f0f30fd40
이준용 <iiiuooyoo@gmail.com>
2023-04-03 13:32:45 +0900

    feat : iPhone 14 Pro Model

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Spline from "@splinetool/react-spline";
+import styled from "@emotion/styled";
+
+const ContainerStyled = styled.div`
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+`;
+
+const SplineContainerStyled = styled.div`
+    width: 100%;
+    height: 50%;
+`;
+
 function App() {
-    return <div>어서오고</div>;
+    return (
+        <ContainerStyled>
+            <Spline scene="https://prod.spline.design/3FnyI600w7sg5Rcv/scene.splinecode" />
+
+            {/* <Spline scene="https://prod.spline.design/bGxpzedyDj3EgoCi/scene.splinecode" /> */}
+        </ContainerStyled>
+    );
 }
 
 export default App;
